23b105e5549d6e97146cec2ed865b2706a0bc1ac
Federira <fedeirar@gmail.com>
2025-02-24 23:47:40 -0300

    feat: added libraries for correction

--- nadro-credit/handler.ts
+++ nadro-credit/handler.ts
@@ -1,6 +1,5 @@
 import { SendEmailNotificationController } from './infrastructure/controllers/SendEmailNotificationController';
 
 export const SendEmailNotification = async (event: any) => {
-  console.log('SendEmailNotification');
   return await SendEmailNotificationController.handle(event);
 };
