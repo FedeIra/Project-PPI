f94889187ab0a59a0adc529b70d79068e97c3649
Federira <fedeirar@gmail.com>
2025-02-24 23:36:49 -0300

    feat: base project executing

--- nadro-credit/handler.ts
+++ nadro-credit/handler.ts
@@ -1,5 +1,6 @@
 import { SendEmailNotificationController } from './infrastructure/controllers/SendEmailNotificationController';
 
 export const SendEmailNotification = async (event: any) => {
+  console.log('SendEmailNotification');
   return await SendEmailNotificationController.handle(event);
 };
